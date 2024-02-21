c150371283394829e8db3ab3358bedecd3033f39
hackerspecter <aicalicapitbull@gmail.com>
2024-02-20 20:00:51 -0400

    reload project

--- dashboard/opendb.tsx
+++ dashboard/opendb.tsx
@@ -1,6 +1,3 @@
-import { Elysia } from "elysia";
-import { html } from '@elysiajs/html' 
-
 import { Database } from "bun:sqlite";
 import {tabelas} from '../dashboard/schema';
 const { setores, prateleiras } = tabelas;
@@ -18,18 +15,3 @@ const query = db.query(`select * from setores  `)
 const setorie: Setor[] = query.all() as Setor[];
 
 console.log(`${setorie[1].setor}`);
-
-  
-new Elysia()
-    .use(html())
-    
-    .get(
-        '/',
-        () => 
-            <div>
-             oi
-            </div>
-    )
-    
-
-    .listen(3000)
---
+++ lego/home.tsx
@@ -0,0 +1,25 @@
+import { Welcomenavbar } from "./pkglego/caixamenu"
+
+export  function Homepg(){
+
+    
+    return(
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href="/css/output.css" rel="stylesheet"></link>
+            <title>Sextbt</title>
+        </head>
+        <body>
+            <div class="container mx-auto">
+
+                <Welcomenavbar/>
+
+            </div>
+
+        </body>
+        </html>
+    )
+}
+
